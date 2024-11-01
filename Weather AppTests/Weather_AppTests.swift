558e6fd227cda0a457c47c083a458b1062753e75
Zeth Thomas <zeththomas89@gmail.com>
2024-11-01 16:46:16 -0700

    Initial Commit

---
+++ Weather AppTests/Weather_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppTests.swift
+//  Weather AppTests
+//
+//  Created by Zeth Thomas on 11/1/24.
+//
+
+import Testing
+@testable import Weather_App
+
+struct Weather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
