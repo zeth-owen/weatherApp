e622d16547bae3a55c4c34cb304a3e1cda38778a
Zeth Thomas <zeththomas89@gmail.com>
2024-11-10 10:40:13 -0800

    various stacks implented, state and binding implementation, and extracted views

--- Weather App/WeatherButton.swift
+++ Weather App/WeatherButton.swift
@@ -5,4 +5,20 @@
 //  Created by Zeth Thomas on 11/1/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
